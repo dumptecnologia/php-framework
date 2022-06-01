9ec339b21723becbbd06fc2372d608fe3b382397
medeirosinacio <medeirosinacio@outlook.com>
2022-06-01 12:24:20 -0300

    update project

--- bin/test.sh
+++ bin/test.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-./vendor/bin/phpunit --testdox tests
+APP_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+cd $APP_PATH/../
+
+docker run -ti --rm  -v  $PWD:/app -w /app --user $(id -u):$(id -g) --dns 8.8.8.8 php:latest php ./vendor/bin/phpunit --testdox tests
