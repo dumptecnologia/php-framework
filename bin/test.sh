effd320f981acfa16c58ac63b6f9afb4d43aefa8
Douglas Medeiros <medeirosinacio@outlook.com>
2022-06-15 18:33:39 -0300

    feat: create TmpFile class

--- bin/test.sh
+++ bin/test.sh
@@ -4,4 +4,4 @@ APP_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 cd $APP_PATH/../
 
-docker run -ti --rm  -v  $PWD:/app -w /app --user $(id -u):$(id -g) --dns 8.8.8.8 php:latest php ./vendor/bin/phpunit --testdox tests
+docker run -ti --rm  -v  $PWD:/app -w /app --user $(id -u):$(id -g) --dns 8.8.8.8 php:latest php ./vendor/bin/phpunit  "$1" --testdox tests
---
+++ tests/Filedir/TmpFileTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Dump\Tests\Filedir;
+
+use Dump\Filedir\TmpFile;
+use PHPUnit\Framework\TestCase;
+
+class TmpFileTest extends TestCase
+{
+
+    public function test_can_create_temporary_file()
+    {
+        $tmpFile = new TmpFile();
+        $tmpFileStatic = TmpFile::new();
+
+        $this->assertFileExists($tmpFile->path);
+        $this->assertFileExists($tmpFileStatic->path);
+    }
+
+    public function test_path_file_has_in_tmp_dir()
+    {
+        $tmpFile = new TmpFile();
+
+        $this->assertEquals('/tmp/', substr($tmpFile->path, 0, 5));
+    }
+
+    public function test_can_delete_tmp_file()
+    {
+        $tmpFile = new TmpFile();
+
+        $tmpFile->remove();
+
+        $this->assertFileDoesNotExist($tmpFile->path);
+    }
+
+    public function test_can_write_tmp_file()
+    {
+        $tmpFile = new TmpFile();
+
+        $tmpFile->write('test');
+
+        $this->assertEquals('test', $tmpFile->getContent());
+    }
+
+    public function test_can_append_tmp_file()
+    {
+        $tmpFile = new TmpFile();
+
+        $tmpFile->write('test');
+        $tmpFile->write('1');
+
+        $this->assertEquals('test1', $tmpFile->getContent());
+    }
+
+
+    public function test_can_preserve_tmp_file_up_to_end_request()
+    {
+        $tmpFile = new TmpFile();
+        $path = $tmpFile->path;
+
+        $tmpFile = null;
+
+        $this->assertFileExists($path);
+    }
+}
