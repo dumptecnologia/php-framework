7fda7051b5c5e4145e5bb040deaaa32f8a230df6
Douglas Medeiros <medeirosinacio@outlook.com>
2022-07-28 17:10:17 -0300

    fix: instance_of type

--- src/Dump/Reflection/Reflection.php
+++ src/Dump/Reflection/Reflection.php
@@ -7,46 +7,46 @@ namespace Dump\Reflection;
  */
 class Reflection
 {
-
-    /**
-     * rewrite the default method to include the enum type
-     *
-     * @param string $class
-     * @param string $method
-     * @return bool
-     */
-    public static function classMethodExist(string $class, string $method): bool
-    {
-        if (enum_exists($class)) {
-            return class_exists($class) && method_exists($class::cases()[0], $method);
-        }
-
-        return class_exists($class) && method_exists($class, $method);
-    }
-
-    /**
-     * Checks if the class or enum instance of
-     *
-     * @param string|object $class
-     * @param string $interface
-     * @return bool
-     */
-    public static function instanceOf(string|object $class, string $interface): bool
-    {
-        if (enum_exists($class)) {
-            return $class::cases()[0] instanceof $interface;
-        }
-
-        return $class instanceof $interface;
-    }
-
-
-    /**
-     * Checks if the class or enum don't instance of
-     * @see instanceOf
-     */
-    public static function dontInstanceOf(string|object $class, string $interface): bool
-    {
-        return !static::instanceOf($class, $interface);
-    }
+	
+	/**
+	 * rewrite the default method to include the enum type
+	 *
+	 * @param string $class
+	 * @param string $method
+	 * @return bool
+	 */
+	public static function classMethodExist(string $class, string $method): bool
+	{
+		if (enum_exists($class)) {
+			return class_exists($class) && method_exists($class::cases()[0], $method);
+		}
+		
+		return class_exists($class) && method_exists($class, $method);
+	}
+	
+	/**
+	 * Checks if the class or enum instance of
+	 *
+	 * @param mixed $class
+	 * @param string $interface
+	 * @return bool
+	 */
+	public static function instanceOf(mixed $class, string $interface): bool
+	{
+		if (enum_exists($class)) {
+			return $class::cases()[0] instanceof $interface;
+		}
+		
+		return $class instanceof $interface;
+	}
+	
+	
+	/**
+	 * Checks if the class or enum don't instance of
+	 * @see instanceOf
+	 */
+	public static function dontInstanceOf(mixed $class, string $interface): bool
+	{
+		return !static::instanceOf($class, $interface);
+	}
 }
--- src/Dump/Reflection/functions.php
+++ src/Dump/Reflection/functions.php
@@ -3,34 +3,34 @@
 use Dump\Reflection\Reflection;
 
 if (!function_exists('class_method_exists')) {
-    /**
-     * Checks if the class and method has been defined
-     */
-    function class_method_exists(string $class, string $method): bool
-    {
-        return Reflection::classMethodExist($class, $method);
-    }
+	/**
+	 * Checks if the class and method has been defined
+	 */
+	function class_method_exists(string $class, string $method): bool
+	{
+		return Reflection::classMethodExist($class, $method);
+	}
 }
 
 
 if (!function_exists('instance_of')) {
-    /**
-     * Checks if the class or enum instance of
-     */
-    function instance_of(string|object $class, string $interface): bool
-    {
-        return Reflection::instanceOf($class, $interface);
-    }
+	/**
+	 * Checks if the class or enum instance of
+	 */
+	function instance_of(mixed $class, string $interface): bool
+	{
+		return Reflection::instanceOf($class, $interface);
+	}
 }
 
 
 if (!function_exists('dont_instance_of')) {
-    /**
-     * Checks if the class or enum don't instance of
-     * @see instance_of
-     */
-    function dont_instance_of(string|object $class, string $interface): bool
-    {
-        return Reflection::dontInstanceOf($class, $interface);
-    }
+	/**
+	 * Checks if the class or enum don't instance of
+	 * @see instance_of
+	 */
+	function dont_instance_of(mixed $class, string $interface): bool
+	{
+		return Reflection::dontInstanceOf($class, $interface);
+	}
 }
