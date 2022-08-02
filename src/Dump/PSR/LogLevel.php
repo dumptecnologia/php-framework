46826822c837793f2ff23e4a66f5fd92c1c43ad8
Douglas Medeiros <medeirosinacio@outlook.com>
2022-08-02 19:11:26 -0300

    feat: LogLevel enum

--- src/Dump/PSR/LogLevel.php
+++ src/Dump/PSR/LogLevel.php
@@ -2,16 +2,14 @@
 
 namespace Dump\PSR;
 
-use Psr\Log\LogLevel as PsrLogLevel;
-
 enum LogLevel: string
 {
-	case EMERGENCY = PsrLogLevel::EMERGENCY;
-	case ALERT = PsrLogLevel::ALERT;
-	case CRITICAL = PsrLogLevel::CRITICAL;
-	case ERROR = PsrLogLevel::ERROR;
-	case WARNING = PsrLogLevel::WARNING;
-	case NOTICE = PsrLogLevel::NOTICE;
-	case INFO = PsrLogLevel::INFO;
-	case DEBUG = PsrLogLevel::DEBUG;
+	case EMERGENCY = 'emergency';
+	case ALERT     = 'alert';
+	case CRITICAL  = 'critical';
+	case ERROR     = 'error';
+	case WARNING   = 'warning';
+	case NOTICE    = 'notice';
+	case INFO      = 'info';
+	case DEBUG     = 'debug';
 }
