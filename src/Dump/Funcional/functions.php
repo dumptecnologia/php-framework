9180502561e026774a60e786166281e66b4eb323
Douglas Medeiros <eu@douglasmedeiros.dev>
2022-09-16 16:04:29 -0300

    feat: add none funcional

--- src/Dump/Funcional/functions.php
+++ src/Dump/Funcional/functions.php
@@ -4,10 +4,21 @@ if (!function_exists('every')) {
     /**
      * Returns true if every value in the collection passes the callback truthy test. Opposite of Functional\none().
      * Callback arguments will be element, index, collection
-     * @ref https://github.com/lstrojny/functional-php/blob/main/docs/functional-php.md#every--invoke
      */
     function every(array|\Traversable $collection, ?callable $callback = null): bool
     {
         return \Functional\every($collection, $callback);
     }
 }
+
+if (!function_exists('none')) {
+    /**
+     * Returns false if every value in the collection passes the callback truthy test. Opposite of Functional\every().
+     * Callback arguments will be element, index, collection
+     */
+    function none(array|\Traversable $collection, ?callable $callback = null): bool
+    {
+        return \Functional\none($collection, $callback);
+    }
+}
+
